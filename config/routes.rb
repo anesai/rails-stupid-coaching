17f27a7cc20a867b4805d62b9e5b63f4b2edf25f
Akane Saito <ukku19@gmail.com>
2023-09-06 16:46:25 +0200

    change ask page to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'ask', to: 'questions#ask'
+  root to: 'questions#ask', as: "ask"
+  # get 'ask', to: 'questions#ask'
+  # ↑　then the browser will be localhost:3000/ask
   get 'answer', to: 'questions#answer'
   # Defines the root path route ("/")
   # root "articles#index"
